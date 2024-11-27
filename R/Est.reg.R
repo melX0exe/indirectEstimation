1e91c8e8a90ef2a44423fcf86a3213d19ea1e333
mels <82780300+melX0exe@users.noreply.github.com>
2024-11-27 10:53:58 +0100

    Update Est.reg.R
    Added two-phased sampling estimators.

--- R/Est.reg.R
+++ R/Est.reg.R
@@ -4,11 +4,13 @@
 #' @param x Vector of data
 #' @param y Vector of auxiliary information
 #' @param mY Mean of auxiliary variable
+#' @param n1 Size of the sample (phase 1)
 #' @param b Slope
 #' @param conf.level Confidence level
+#' @param twophase Indicates whether double sampling was used
 #' @export
 
-Est.reg <- function(x, y, N, mY, b=NA, conf.level=0.95){
+Est.reg <- function(x, y, N, mY, n1, b=NA, conf.level=0.95, twophase=FALSE){
   
   n=length(x) # sample size
   s2x=var(x) # sample variance (x)
@@ -23,31 +25,66 @@ Est.reg <- function(x, y, N, mY, b=NA, conf.level=0.95){
   e.m <- mean(x) + b * (mY - mean(y)) # Mean
   e.t <- e.m * N # Total
   
-  # Variance of the estimators
-  v.m <- ((N-n)*s2x*(1-rho^2))/(N*n) # Mean
-  v.t <- N^2*v.m # Total
+  if(twophase == FALSE){ # We know the mean of the population for Y
+    
+    # Variance of the estimators
+    v.m <- ((N-n)*s2x*(1-rho^2))/(N*n) # Mean
+    v.t <- N^2*v.m # Total
+    
+    # Errors
+    er.m=sqrt(v.m) # Mean
+    er.t=sqrt(v.t) # Total
+    
+    # Confidence interval
+    # We assume bias = 0:
+    # Mean
+    lower.m <- e.m - qnorm(1-(1-conf.level)/2) * er.m
+    upper.m <- e.m + qnorm(1-(1-conf.level)/2) * er.m
+    # Total
+    lower.t <- e.t - qnorm(1-(1-conf.level)/2) * er.t
+    upper.t <- e.t + qnorm(1-(1-conf.level)/2) * er.t
+    
+    return(list(Est.b=b,
+         Est.media=e.m,
+         Est.total=e.t,
+         Var.media=v.m,
+         Var.total=v.t,
+         Err.m=er.m,
+         Err.t=er.t,
+         mean.conf.int = c(lower.m,upper.m),
+         total.conf.int = c(lower.t,upper.t)
+    ))
+    
+  } else {
+    
+    # Variance of the estimators
+    v.m <- (N-n1)*s2x/(N*n1) + (n1-n)/(n1*n)*(1-rho^2)*s2x # Mean
+    v.t <- N^2*v.m # Total
+    
+    # Errors
+    er.m=sqrt(v.m) # Mean
+    er.t=sqrt(v.t) # Total
+    
+    # Confidence interval
+    # We assume bias = 0:
+    # Mean
+    lower.m <- e.m - qnorm(1-(1-conf.level)/2) * er.m
+    upper.m <- e.m + qnorm(1-(1-conf.level)/2) * er.m
+    # Total
+    lower.t <- e.t - qnorm(1-(1-conf.level)/2) * er.t
+    upper.t <- e.t + qnorm(1-(1-conf.level)/2) * er.t
+    
+    return(list(Est.b=b,
+         Est.media=e.m,
+         Est.total=e.t,
+         Var.media=v.m,
+         Var.total=v.t,
+         Err.m=er.m,
+         Err.t=er.t,
+         mean.conf.int = c(lower.m,upper.m),
+         total.conf.int = c(lower.t,upper.t)
+    ))
+    
+  }
   
-  # Errors
-  er.m=sqrt(v.m) # Mean
-  er.t=sqrt(v.t) # Total
-  
-  # Confidence interval
-  # We assume bias = 0:
-  # Mean
-  lower.m <- e.m - qnorm(1-(1-conf.level)/2) * er.m
-  upper.m <- e.m + qnorm(1-(1-conf.level)/2) * er.m
-  # Total
-  lower.t <- e.t - qnorm(1-(1-conf.level)/2) * er.t
-  upper.t <- e.t + qnorm(1-(1-conf.level)/2) * er.t
-  
-  list(Est.b=b,
-       Est.media=e.m,
-       Est.total=e.t,
-       Var.media=v.m,
-       Var.total=v.t,
-       Err.m=er.m,
-       Err.t=er.t,
-       mean.conf.int = c(lower.m,upper.m),
-       total.conf.int = c(lower.t,upper.t)
-       )
 }
